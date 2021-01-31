5deeea96e4632ba4a2a1d853b239b3335f1d9cff
joy012 <joy.buet.012@gmail.com>
2021-02-01 03:34:03 +0600

    alert added

--- script.js
+++ script.js
@@ -2,39 +2,45 @@
 
 document.getElementById('preview').style.display = 'none';
 
-document.getElementById('from').addEventListener('blur', function (e){
-    document.getElementById('fromAddress').innerHTML =  getValue(e);
+document.getElementById('from').addEventListener('blur', function (e) {
+    document.getElementById('fromAddress').innerHTML = getValue(e);
 });
-document.getElementById('to').addEventListener('blur', function (e){
-    document.getElementById('toAddress').innerHTML =  getValue(e);
+document.getElementById('to').addEventListener('blur', function (e) {
+    document.getElementById('toAddress').innerHTML = getValue(e);
 });
-document.getElementById('fromDate').addEventListener('change', function (e){
-    document.getElementById('departure').innerHTML =  getValue(e);
+document.getElementById('fromDate').addEventListener('change', function (e) {
+    document.getElementById('departure').innerHTML = getValue(e);
 });
-document.getElementById('toDate').addEventListener('change', function (e){
-    document.getElementById('return').innerHTML =  getValue(e);
+document.getElementById('toDate').addEventListener('change', function (e) {
+    document.getElementById('return').innerHTML = getValue(e);
 });
 
 
-function book(){
-    document.getElementById('form').style.display = 'none';
-    document.getElementById('preview').style.display = 'block';
+function book() {
+    var isConfirm = confirm('You are about to book your ticket. Press "ok" to continue')
+    if (isConfirm) {
+        document.getElementById('form').style.display = 'none';
+        document.getElementById('preview').style.display = 'block';
+    }
+    else{
+        alert('Please Provide all information');
+    }
 }
 function getValue(e) {
     return e.target.value;
 }
 
 
-function ticketCount(ticket,isIncrease) {
+function ticketCount(ticket, isIncrease) {
     var currentCount = parseInt(document.getElementById(ticket + '-count').value);
-    
-    if(isIncrease){
-       currentCount += 1;
+
+    if (isIncrease) {
+        currentCount += 1;
     }
-    else{
-       if(currentCount > 0){
-           currentCount -= 1;
-       }
+    else {
+        if (currentCount > 0) {
+            currentCount -= 1;
+        }
     }
     document.getElementById(ticket + '-count').value = currentCount;
     document.getElementById(ticket + '-seat').innerHTML = currentCount;
@@ -42,12 +48,12 @@ function ticketCount(ticket,isIncrease) {
     ticketCost();
 }
 
-function ticketCost (){
+function ticketCost() {
     var subtotal = parseInt(document.getElementById('subtotal').innerHTML);
     var firstClass = parseInt(document.getElementById('firstclass-count').value);
     var economyClass = parseInt(document.getElementById('economy-count').value);
     subtotal = (firstClass * 150) + (economyClass * 100);
-    var vat = subtotal * 0.15 ;
+    var vat = subtotal * 0.15;
     var total = subtotal + vat;
     document.getElementById('subtotal').innerHTML = subtotal;
     document.getElementById('vat').innerHTML = vat.toFixed(2);
